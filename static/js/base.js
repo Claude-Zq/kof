85a5068bb539af73e8eb7786e39f3cc778d6c2e7
Claude-Zq <1574089391@qq.com>
2022-10-31 10:37:03 +0800

    add background

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,15 @@
 export class KOF{
     constructor(id){
         this.$kof = $('#'+id);
+
+        $('#'+id).css({
+            "width":window.innerWidth,
+            "height":window.innerHeight,
+            "background-image": "url('/static/images/background/0.gif')",
+            "background-size": "100% 100%",
+            "background-position": "top",
+            "position": "absolute",
+        })
     }
+
 }

6ca104d6b1f675ad8bae35e6b57e3e0595efbb4e
Claude-Zq <1574089391@qq.com>
2022-10-31 14:23:10 +0800

    add gravity

--- static/js/player/base.js
+++ static/js/player/base.js
@@ -1,6 +1,5 @@
 import { GameObject } from "../game_object/base.js";
-
-
+import { GRAVITY } from "../base.js";
 
 export class Player extends GameObject{
     constructor(root,info) {
@@ -10,10 +9,17 @@ export class Player extends GameObject{
         this.id = info.id;
         this.x = info.x;
         this.y = info.y;
+        this.speedx = info.speedx;
+        this.speedy = info.speedy;
         this.width = info.width;
         this.height = info.height;
 
-        
+        this.vx = 0;
+        this.vy = 0;
+
+       
+
+
         
         this.ctx = this.root.gameMap.ctx;
         
@@ -24,7 +30,19 @@ export class Player extends GameObject{
 
     }
 
+    update_move(){
+        this.vy += GRAVITY;
+        this.x +=this.vx* this.timedelta/1000;
+        this.y += this.vy * this.timedelta/1000;
+       
+        if(this.y > this.ctx.canvas.height-this.height-50){
+            this.y = this.ctx.canvas.height - this.height-50;
+            this.vy = 0;
+        }
+    }
+
     update(){
+        this.update_move();
         this.render();
     }
 

6ac3a0ac6502c56d37a406647f77530dd9ce82dc
Claude-Zq <1574089391@qq.com>
2022-11-06 15:31:47 +0800

    add gameIntro

--- static/js/menu/base.js
+++ static/js/menu/base.js
@@ -1,30 +1,30 @@
 export class Menu{
     constructor(root) {
         this.root = root;
+
+        //开始菜单页
         let startMenu = $(`
         <div id="start-menu">
 		    <h1>大乱斗</h1>
             <!-- 选择按钮 --> 
             <div id='menu'>
-                <button id="select-role">切换角色</button>
-                <button id="select-background">切换背景</button>
-                <button id="intro">游戏介绍</button>
-                <button id ="start">开始游戏</button>
+                <button id="select-role-btn">切换角色</button>
+                <button id="select-background-btn">切换背景</button>
+                <button id="intro-btn">游戏介绍</button>
+                <button id ="start-btn">开始游戏</button>
             </div>
 	    </div>`
         )
+
+        //介绍页面
         let intro = $(` 
         <!-- 游戏介绍 -->
-        <div class="intro">
-            <h2>游戏规则：</h2>
+        <div id="intro">
+            <h2>游戏介绍</h2>
             <p>
-                这是非常简单的点击小游戏<br>
-                您只需要点击星星就可以获得分数<br>
-                <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fgss0.baidu.com%2F94o3dSag_xI4khGko9WTAnF6hhy%2Fzhidao%2Fpic%2Fitem%2F960a304e251f95ca31067fcbc9177f3e660952d8.jpg&refer=http%3A%2F%2Fgss0.baidu.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1669965862&t=eedab0831a655406dc30114eb62f3014" width="50"><br>
-                您每次点击星星进度条都会减短<br>
-            <div class='bar'><span></span></div>
-            进度条满后游戏结束
-            <div class='bar'><span style="width: 100%"></span></div>
+                简单的双人对战游戏<br>
+                <br>
+                <br>
             </p>
             <button>X</button>
         </div>`)
@@ -33,27 +33,24 @@ export class Menu{
         intro.hide();
 
         // 规则按钮
-        $("#menu #intro").click(function () {
-            $(".intro").show();
-            $('#start-menu').hide();
+        $("#menu #intro-btn").click(function () {
+            $("#intro").show();
+            // $('#start-menu').hide();
         })
 
-        $('.intro button').click(function () {
-            $(".intro").hide();
+        $('#intro button').click(function () {
+            $("#intro").hide();
             $('#start-menu').show();
         })
 
         // 开始 按钮
-        $("#menu #start").click(function () {
+        $("#menu #start-btn").click(function () {
             $("#start-menu").fadeOut(500);
             root.startGame();// 开始游戏
         });
 
-        $("#menu #select-background").click(function(){
-            // $("#start-menu").fadeOut(500);
+        $("#menu #select-background-btn").click(function(){
             root.bgId = (root.bgId+1)%5;
-            console.log(root.bgId);
-            // root.startGame();// 开始游戏
             $('#start-menu').css({
                 "background-image": `url("/static/images/background/${root.bgId}.gif")`
             })    
@@ -80,7 +77,7 @@ export class Menu{
         //     text-align: center;
         // }
         //整个菜单
-        $('#start-menu').css({
+        $(`#${this.root.id} #start-menu`).css({
             "width":window.innerWidth,
             "height":window.innerHeight,
             "background-image": `url("/static/images/background/${this.root.bgId}.gif")`,
@@ -138,20 +135,7 @@ export class Menu{
 
     }
 
-
     setIntroCss(){
-
-    $(`#${this.root.id} #menu button`).css({
-        "display ": "inline-block",
-        "background-color":"#1f9cf0",
-        "box-shadow": "0px 0px 20px rgb(89, 175, 255)",
-        "width": "300px",
-        "height": "60px",
-        "border-radius": "20px",
-        "margin": "20px 0",
-        "color": "white",
-        "border": "none",
-    })
 
 
 // /* 规则盒子 */
@@ -173,26 +157,41 @@ export class Menu{
 //     line-height: 40px;
 // }
 
-// /* 规则 关闭按钮 */
-// .guize-box button {
-//     position: absolute;
-//     right: 0;
-//     top: 0;
-//     width: 40px;
-//     height: 40px;
-//     border-radius: 20px;
-//     border: none;
-//     font-weight: 700;
-//     font-size: 24px;
-//     color: #fff;
-//     background-color: transparent;
-// }
+    $(`#${this.root.id} #intro`).css({
+        // "display": "none",
+        "position": "absolute",
+        "left": "50%",
+        "top": "50%",
+        "transform": "translate(-50%, -50%)",
+        "background-image": "linear-gradient(160deg, rgb(89, 175, 255), rgb(99, 63, 255))",
+        "box-shadow": "0px 0px 20px rgb(89, 175, 255)",
+        "width": "300px",
+        "height": "300px",
+        "border-radius": "20px",
+        "text-align": "center",
+        "padding": "40px",
+        "color": "#fff",
+        "overflow": "hidden",
+        "line-height": "40px",
+    })
 
-// /* 规则 文本 */
-// .guize-box p {
-//     font-size: 18px;
-// }
+    $(`#${this.root.id} #intro button`).css({
+        "position": "absolute",
+        "right": "0",
+        "top": "0",
+        "width": "40px",
+        "height": "40px",
+        "border-radius": "20px",
+        "border": "none",
+        "font-weight": "700",
+        "font-size": "24px",
+        "color": "#fff",
+        "background-color": "transparent", 
+    })
 
+    $(`#${this.root.id} #intro p`).css({
+        "font-size": "20px",
+    })
     }
 
 
